cae7ba37d884887f5a15e5f9e20df20ece7c8126
Dilip <dilip.884400@gmail.com>
2023-05-04 21:33:58 +0530

    assignment done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from "react";
 
-function App() {
+function Calendar() {
+  const [date, setDate] = useState(new Date());
+
+  function handlePrevMonth() {
+    setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() - 1, 1));
+  }
+
+  function handleNextMonth() {
+    setDate(prevDate => new Date(prevDate.getFullYear(), prevDate.getMonth() + 1, 1));
+  }
+
+  const daysInMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+  const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+
+  const days = [];
+  for (let i = 1; i <= daysInMonth; i++) {
+    days.push(i);
+  }
+
+  const emptyDays = [];
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    emptyDays.push(i);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calendar-container">
+    <h2>{date.toLocaleString("default", { month: "long", year: "numeric" })}</h2>
+    <div className="calendar-buttons">
+      <button onClick={handlePrevMonth}>Prev</button>
+      <button onClick={handleNextMonth}>Next</button>
     </div>
+    <table className="calendar-table">
+      <thead>
+        <tr>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          <th>Sun</th>
+          <th>Mon</th>
+          <th>Tue</th>
+          <th>Wed</th>
+          <th>Thu</th>
+          <th>Fri</th>
+          <th>Sat</th>
+          <th>Sun</th>
+          <th>Mon</th>
+        </tr>
+      </thead>
+      <tbody>
+        {emptyDays.map(() => <td className="calendar-empty"></td>)}
+        {days.map(day => <td className="calendar-day">{day}</td>)}
+      </tbody>
+    </table>
+  </div>
   );
 }
 
-export default App;
+export default Calendar;
