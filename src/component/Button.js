b906db7c55cbcebde7a43c34bf29ac2d5bb786f6
Dilip <dilip.884400@gmail.com>
2023-05-10 00:07:34 +0530

    button assignment done

---
+++ src/component/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Button(props) {
+  return (
+    <div >
+      <button onClick={props.onClick}>{props.children}</button>
+    </div>
+  );
+}
